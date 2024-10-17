dadd5d295dc5bc26773aedb5e7d5652ded2c6b49
BloomingSakura <blooming.sakura10@gmail.com>
2024-10-17 12:23:12 +0200

    Switch

--- GRa/Program.cs
+++ GRa/Program.cs
@@ -2,6 +2,36 @@
 {
     static void Main(string[] args)
     {
+        Random rnd = new Random();
+        int wybor_komputera = rnd.Next(3);
 
+        Console.WriteLine("Witaj w grze Kamień,papier nożyce! Zmierz się z naszym komputerem");
+        Console.Write("Co wybierasz: kamień (k), papier (p) czy nożyce (n): ");
+        string wybor_gracza = Console.ReadLine();
+
+        int wybor_gracza_int;
+        switch(wybor_gracza)
+        {
+            case "k":
+            {
+                wybor_gracza_int = 0;
+                break;
+            }
+            case "p":
+            {
+                wybor_gracza_int = 1;
+                break;
+            }
+            case "n":
+            {
+                wybor_gracza_int = 2;
+                break;
+            }
+            default:
+            {
+                    Console.WriteLine("Zła odpowiedź");
+                    break;
+            }
+        }
     }
 }
